3afcdc4d1f5a4ea02c6671f8fc7c36d618d85f63
harrison-symes <symeshjb@gmail.com>
2017-10-30 12:17:48 +1300

    can resume progress. Saves dex number, name, image and description

--- getPokemons.js
+++ getPokemons.js
@@ -2,8 +2,8 @@ var request = require('superagent')
 var cheerio = require('cheerio')
 
 request
-  .get('https://www.pokemon.com/us/pokedex/')
+  .get('https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number#List_of_Pok.C3.A9mon_by_National_Pok.C3.A9dex_number')
   .then(res => {
     const $ = cheerio.load(res.text)
-    console.log($('.id'));
+    console.log($('table').find('tbody').find('tr').find('td').find('a')[0])
   })
--- poke.js
+++ poke.js
@@ -35,16 +35,30 @@ function writeFile(pokemon) {
   });
 }
 
+const getImage = ($) => $('.profile-images').find('img')[0].attribs.src
+
+const getName = ($)  => $('.pokedex-pokemon-pagination-title')[0].children[1].children[0].data.split(' ').filter(c =>c!=' ').join('').split('\n')[1]
+
+const getNumber = ($) => $('.pokedex-pokemon-pagination-title')[0].children[1].children[1].children[0].data.split('#')[1]
+
+const getDescription = ($) =>  $('.version-y')[0].children[0].data.split(' ').filter(c => c!= '').join(' ').split('\n').filter(c=>c!='').join(' ').split('').filter((a,b,c) => b!=0).join('')
+
+
+const getPokemon = ($) => ({
+  image: getImage($),
+  number: getNumber($),
+  name: getName($),
+  description: getDescription($)
+})
+
 function getImageRecursive (idx, arr) {
   return new Promise(function(resolve, reject) {
+    console.log(idx)
     request
     .get('https://www.pokemon.com/us/pokedex/' + idx)
     .then(res => {
       var $ = cheerio.load(res.text)
-      var pokemon = {
-        image: $('.profile-images').find('img')[0].attribs.src
-
-      }
+      var pokemon = getPokemon($)
       arr.push(pokemon)
       writeFile(arr)
         .then(message => console.log(message))
@@ -55,6 +69,9 @@ function getImageRecursive (idx, arr) {
       else resolve(getImageRecursive(idx + 1, arr))
       // console.log(res.text);
     })
-    .catch(err => resolve(getImageRecursive(idx, arr)))
+    .catch(err => {
+      console.log(err)
+      resolve(getImageRecursive(idx, arr))
+    })
   })
 }
